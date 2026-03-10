74a78513f305d754e5b68ea661dfa6e7b8d8bca4
Bilal Ahmad <bilal.codemaster@gmail.com>
2026-03-10 21:15:50 +0500

    pushed

--- app/lib/axios.js
+++ app/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    process.env.NEXT_PUBLIC_API_URL,
+    process.env.NEXT_PUBLIC_API_URL || "https://quiz-system-1ds3.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
